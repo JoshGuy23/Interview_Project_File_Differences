2b37496e2266816f2626705840014b2f6308fa99
joshua <jhecker2001@gmail.com>
2024-05-09 14:34:16 -0500

    Added ability to test for differences in files on small scale.

--- tar_transaction.py
+++ tar_transaction.py
@@ -5,12 +5,12 @@ class Tar:
         self.charge = 0.0
         self.new_charge = 0.0
 
-    def get_spa(self, new_spa):
+    def set_spa(self, new_spa):
         self.spa = str(new_spa)
 
-    def get_service_code(self, new_code):
+    def set_service_code(self, new_code):
         self.service_code = new_code
 
-    def get_charges(self, c, n_c):
+    def set_charges(self, c, n_c):
         self.charge = float(c)
         self.new_charge = float(n_c)
