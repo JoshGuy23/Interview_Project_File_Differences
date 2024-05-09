bbe7b0c92cda1b5008bec4ec704c330926fc5296
joshua <jhecker2001@gmail.com>
2024-05-09 13:34:38 -0500

    Added methods to add the required data into the classes.

---
+++ tar_transaction.py
@@ -0,0 +1,16 @@
+class Tar:
+    def __init__(self):
+        self.spa = ""
+        self.service_code = ""
+        self.charge = 0.0
+        self.new_charge = 0.0
+
+    def get_spa(self, new_spa):
+        self.spa = str(new_spa)
+
+    def get_service_code(self, new_code):
+        self.service_code = new_code
+
+    def get_charges(self, c, n_c):
+        self.charge = c
+        self.new_charge = n_c
