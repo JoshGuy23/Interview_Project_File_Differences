d4b737eab0dd0faddf2f6c0496fbb78340942562
joshua <jhecker2001@gmail.com>
2024-05-10 15:32:22 -0500

    Overhauled system: Both input files are merged, and intertuples is used to iterate over the rows. Class use no longer needed.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from pandas import ExcelWriter, ExcelFile
 import openpyxl
-from row_data import Row
 
 # Notes:
 # tar file: SPA is row.iloc[0], service code is 1, charge is 2, new charge is 4
@@ -17,120 +16,48 @@ e_new_charge_list = []
 
 
 # Read in the tar and ecb files
-tar_file = pd.read_excel('ServiceCodes_TAR.xlsx', skiprows=2, nrows=10, index_col=None)
-ecb_file = pd.read_excel('ServiceCodes_ECB.xlsx', skiprows=2, nrows=10, index_col=None)
+tar_file = pd.read_excel('ServiceCodes_TAR.xlsx', skiprows=1, index_col=None)
+ecb_file = pd.read_excel('ServiceCodes_ECB.xlsx', skiprows=1, index_col=None)
+all_entries = ecb_file.merge(tar_file, on=['SPA', 'Service Code'], how='outer')
 
-# Set up the classes for comparison.
-tar_row = Row()
-ecb_row = Row()
-
-for index, row in tar_file.iterrows():
-    match = False
-
-    # For each row in the tar file, put in the relevant data for that row
-    tar_row.set_spa(row.iloc[0])
-    tar_row.set_service_code(row.iloc[1])
-    tar_row.set_charges(row.iloc[2], row.iloc[4])
-
-    # Go through each row of the ecb file to find a match
-    for ecb_index, e_row in ecb_file.iterrows():
-        # Add each ecb row into the relevant class
-        ecb_row.set_spa(e_row.iloc[0])
-        ecb_row.set_service_code(e_row.iloc[1])
-        ecb_row.ecb_set_charges(e_row.iloc[3], e_row.iloc[5])
-
-        if tar_row.spa == ecb_row.spa and tar_row.service_code == ecb_row.service_code:
-            diff_charges = False
-            diff_new_charges = False
-
-            # If a row with the matching spa and code is found, compare charges
-            match = True
-
-            if tar_row.charge != ecb_row.charge:
-                diff_charges = True
-
-            if tar_row.new_charge != ecb_row.new_charge:
-                diff_new_charges = True
-
-            # Append differing data to list to go into Excel file.
-            if diff_charges and diff_new_charges:
-                spa_list.append(tar_row.spa)
-                service_code_list.append(tar_row.service_code)
-
-                error_list.append("Differing charges and new charges")
-
-                t_charge_list.append(f"{tar_row.charge}")
-                e_charge_list.append(f"{ecb_row.charge}")
-
-                t_new_charge_list.append(f"{tar_row.new_charge}")
-                e_new_charge_list.append(f"{ecb_row.new_charge}")
-            elif diff_charges:
-                spa_list.append(tar_row.spa)
-                service_code_list.append(tar_row.service_code)
-
-                error_list.append("Differing charges")
-
-                t_charge_list.append(f"{tar_row.charge}")
-                e_charge_list.append(f"{ecb_row.charge}")
-
-                t_new_charge_list.append("N/A")
-                e_new_charge_list.append("N/A")
-            elif diff_new_charges:
-                spa_list.append(tar_row.spa)
-                service_code_list.append(tar_row.service_code)
-
-                error_list.append("Differing new charges")
-
-                t_charge_list.append("N/A")
-                e_charge_list.append("N/A")
-
-                t_new_charge_list.append(f"{tar_row.new_charge}")
-                e_new_charge_list.append(f"{ecb_row.new_charge}")
-
-            # Exit the inner loop
-            break
-
-    # If no match was found, report an error
-    if not match:
-        spa_list.append(tar_row.spa)
-        service_code_list.append(tar_row.service_code)
-
+for row in all_entries.itertuples():
+    if pd.isnull(row[4]) or pd.isnull(row[10]):
+        spa_list.append(row[1])
+        service_code_list.append(row[2])
         error_list.append("Entry not in both files")
-
         t_charge_list.append("N/A")
         e_charge_list.append("N/A")
-
         t_new_charge_list.append("N/A")
         e_new_charge_list.append("N/A")
+    else:
+        ecb_charge = float(row[4][1:])
+        if ecb_charge != float(row[10]) and float(row[6]) != float(row[12]):
+            spa_list.append(row[1])
+            service_code_list.append(row[2])
+            error_list.append("Differing charges and new charges")
+            e_charge_list.append(f"{ecb_charge}")
+            t_charge_list.append(f"{float(row[10])}")
+            e_new_charge_list.append(f"{float(row[6])}")
+            t_new_charge_list.append(f"{float(row[12])}")
+        elif ecb_charge != float(row[10]):
+            spa_list.append(row[1])
+            service_code_list.append(row[2])
+            error_list.append("Differing charges")
+            t_charge_list.append(f"{float(row[10])}")
+            e_charge_list.append(f"{ecb_charge}")
+            t_new_charge_list.append("N/A")
+            e_new_charge_list.append("N/A")
+        elif float(row[6]) != float(row[12]):
+            spa_list.append(row[1])
+            service_code_list.append(row[2])
+            error_list.append("Differing new charges")
+            t_charge_list.append("N/A")
+            e_charge_list.append("N/A")
+            t_new_charge_list.append(f"{float(row[12])}")
+            e_new_charge_list.append(f"{float(row[6])}")
 
-# Check if entries in ecb file not in tar file
-for index, row in ecb_file.iterrows():
-    match = False
 
-    ecb_row.set_spa(row.iloc[0])
-    ecb_row.set_service_code(row.iloc[1])
-
-    for tar_index, t_row in tar_file.iterrows():
-        tar_row.set_spa(t_row.iloc[0])
-        tar_row.set_service_code(t_row.iloc[1])
-
-        if tar_row.spa == ecb_row.spa and tar_row.service_code == ecb_row.service_code:
-            match = True
-            # Exit inner loop if match is found
-            break
-
-    if not match:
-        spa_list.append(ecb_row.spa)
-        service_code_list.append(ecb_row.service_code)
-
-        error_list.append("Entry not in both files")
-
-        t_charge_list.append("N/A")
-        e_charge_list.append("N/A")
-
-        t_new_charge_list.append("N/A")
-        e_new_charge_list.append("N/A")
-
+print("Done finding differences. Now writing to file.")
 # Write data to Excel file
 df = pd.DataFrame({'SPA': spa_list,
                    'Service Code': service_code_list,
@@ -143,6 +70,8 @@ df = pd.DataFrame({'SPA': spa_list,
 writer = ExcelWriter('ServiceCodes_DifferenceLog.xlsx')
 df.to_excel(writer, sheet_name='Sheet1', index=False)
 writer.close()
+
+print("Done writing to file.")
 
 # Some testing code to determine how to access needed information
 # ecb_file = pd.read_excel('ServiceCodes_ECB.xlsx', skiprows=2, nrows=10, index_col=None)
